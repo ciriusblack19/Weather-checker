71b50e0ad0a9b1edbdcf59456056d520dfd80d9a
Jose Antonio <ciriusblack@MacBook-Air-di-Jose.local>
2023-04-14 00:01:31 +0200

    Added some css to Weather component and a dinamic background to App component

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -5,21 +5,32 @@ import { getWeatherData } from "../../utils/api";
 const Weather = ({ onGetCityImage }) => {
     const [city, setCity] = useState('');
     const [weatherData, setWeatherData] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const data = await getWeatherData(city);
-        setWeatherData(data);
-        onGetCityImage(city);
+
+        if (data.cod === "404") {
+            setWeatherData(null);
+            setErrorMessage(`Spiacenti, la città inserita non è stata trovata`);
+        } else {
+            setWeatherData(data);
+            setErrorMessage('');
+            onGetCityImage(city);
+        }
     };
 
     return (
         <div>
-            <h2>Inserisci il nome della città</h2>
+            <h2 className='intro'>Inserisci il nome della città</h2>
             <form onSubmit={handleSubmit}>
-                <input type="text" value={city} onChange={(e) => setCity(e.target.value)} className='input-city' />
+                <input type="text" value={city} onChange={(e) => setCity(e.target.value)} className='input-city' placeholder='es. Parigi' />
                 <button type="submit" className='search-button'>Cerca</button>
             </form>
+            {errorMessage && (
+                <p className='error-message'>{errorMessage}</p>
+            )}
             {weatherData && (
                 <div className="weather-info">
                     <h2 className="city-name">{weatherData.name}</h2>
