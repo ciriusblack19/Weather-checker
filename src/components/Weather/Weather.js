ef7bb574b4a251e3812ddeb4fa5e259558da24b1
Jose Antonio <ciriusblack@MacBook-Air-di-Jose.local>
2023-04-13 14:42:36 +0200

    Removed useEffect from Weather component

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,5 +1,5 @@
 import './Weather.css';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { getWeatherData } from "../../utils/api";
 
 const Weather = ({ onGetCityImage }) => {
